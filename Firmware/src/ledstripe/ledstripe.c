713b15db9cb4b1bd210c403b00008b0ac30c2454
c3ma <c3ma@calculator>
2014-10-24 22:04:25 +0200

    Starting

--- Firmware/src/ledstripe/ledstripe.c
+++ Firmware/src/ledstripe/ledstripe.c
@@ -17,6 +17,10 @@
 void
 ledstripe_init(void)
 {
+	/* Allocating two DMA2 streams for memory copy operations.*/
+	if (dmaStreamAllocate(STM32_DMA2_STREAM6, 0, NULL, NULL))
+		chSysHalt();
 
+	dmaStreamSetPeripheral(STM32_DMA2_STREAM6,
 }
 
