c3850c7b7fc643f2fe15518b2a93c328813f9b99
c3ma <c3ma@calculator>
2014-11-08 20:40:03 +0100

    Different signal periods of high levels are possible

--- Firmware/src/ledstripe/ledstripe.c
+++ Firmware/src/ledstripe/ledstripe.c
@@ -11,27 +11,46 @@
 
 #include "ledstripe/ledstripe.h"
 
-#define		BIT_LOW 	30	/*FIXME adapt values */
-#define		BIT_HIGH 	50	/*FIXME adapt values */
+#define		BIT_LOW 	30	/*FIXME adapt values (in micro seconds) */
+#define		BIT_HIGH 	50	/*FIXME adapt values (in micro seconds) */
+
+#define		BIT_LENGTH	100	/* micro seconds */
 
 #define TEST_PATTERN_LENGTH		8
 
+#define	 set_pin_high()	palSetPad(GPIOD, GPIOD_LED5) 	/* Red.  */
+#define set_pin_low()	palClearPad(GPIOD, GPIOD_LED5) 	/* Red.  */
+
 static uint8_t testpattern[TEST_PATTERN_LENGTH] = { BIT_HIGH, BIT_HIGH, BIT_HIGH, BIT_HIGH, BIT_LOW, BIT_LOW, BIT_LOW, BIT_LOW };
 
-static int lastCycle=BIT_LOW;
+static int byteOffset=0;	/* is used with the doulbed size of "testpattern" to simulate the value to fall */
 
 static void gpt_adc_trigger(GPTDriver *gpt_ptr)
 {
-	palTogglePad(GPIOD, GPIOD_LED5);	/* Red.  */
-
-	// demo simply switch between the two cylce times
-	if (lastCycle == BIT_LOW)
-		lastCycle = BIT_HIGH;
+	/* Update next value */
+	if ((byteOffset / 2) >= TEST_PATTERN_LENGTH)
+	{
+		/*FIXME normal, the next must be copied, now simply restart */
+		byteOffset = 0;
+	}
 	else
-		lastCycle = BIT_LOW;
+	{
+		byteOffset++;
+	}
 
-	// restart timer
-	gptStartOneShot(gpt_ptr, lastCycle);
+	if (byteOffset % 2 != 0)
+	{
+		set_pin_low();
+
+		// restart timer
+		gptStartOneShot(gpt_ptr, BIT_LENGTH - testpattern[byteOffset / 2] );
+	}
+	else	/* go to the next field in the array */
+	{
+		set_pin_high();
+		gptStartOneShot(gpt_ptr, testpattern[byteOffset / 2] );
+	}
+
 }
 
 static uint8_t* dma_source;
@@ -62,7 +81,8 @@ void ledstripe_init() {
 	  * Timer is clocked at 1Mhz (1us). Timer triggers at 76us and calls the callback function
 	  */
 	gptStart(&GPTD1, &gpt_adc_config);
-	gptStartOneShot(&GPTD1, lastCycle);
+	set_pin_high();
+	gptStartOneShot(&GPTD1, testpattern[byteOffset] /* begin with the first time */);
 
 }
 
