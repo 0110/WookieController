7a26b18d0a3d137f097fab5460407bb9c41a53ca
c3ma <c3ma@calculator>
2014-11-08 16:10:16 +0100

    PWM is working

--- Firmware/src/ledstripe/ledstripe.c
+++ Firmware/src/ledstripe/ledstripe.c
@@ -12,11 +12,34 @@
  * GLOBAL FUNCTIONS
  ******************************************************************************/
 
-
+static PWMConfig pwmcfg = {
+10000, /* 10Khz PWM clock frequency */
+1000, /* PWM period 1 second */
+NULL, /* No callback */
+/* Only channel 4 enabled */
+{
+{PWM_OUTPUT_DISABLED, NULL},
+{PWM_OUTPUT_DISABLED, NULL},
+{PWM_OUTPUT_DISABLED, NULL},
+{PWM_OUTPUT_ACTIVE_HIGH, NULL},
+},
+0
+};
 
 void
 ledstripe_init(void)
 {
+
+	/* Enables PWM output (of TIM4, channel 4) on blue LED connected to PD15 */
+	palSetPadMode(GPIOD, GPIOD_LED6, PAL_MODE_ALTERNATE(2));
+
+	pwmStart(&PWMD4, &pwmcfg);
+	pwmEnableChannel(&PWMD4, 3, 500);
+
+	while (TRUE) {
+		chThdSleepMilliseconds(5000);
+	}
+
 #if 0
 	/* Allocating two DMA2 streams for memory copy operations.*/
 	if (dmaStreamAllocate(STM32_DMA2_STREAM6, 0, (stm32_dmaisr_t) _dma_rotate_callback, NULL))
