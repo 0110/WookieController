ed3191f4d2ea1384a4f0b416ba68b2610697cd43
c3ma <c3ma@calculator>
2014-11-08 15:57:06 +0100

    Cleaned project - starting PWM implementation

--- Firmware/src/ledstripe/ledstripe.c
+++ Firmware/src/ledstripe/ledstripe.c
@@ -1,7 +1,7 @@
 #include "ch.h"
 #include "hal.h"
-#include "ledstripe/ledstripe.h"
 
+#include "ledstripe/ledstripe.h"
 
 /******************************************************************************
  * PROTOTYPE
@@ -17,10 +17,24 @@
 void
 ledstripe_init(void)
 {
+#if 0
 	/* Allocating two DMA2 streams for memory copy operations.*/
-	if (dmaStreamAllocate(STM32_DMA2_STREAM6, 0, NULL, NULL))
+	if (dmaStreamAllocate(STM32_DMA2_STREAM6, 0, (stm32_dmaisr_t) _dma_rotate_callback, NULL))
 		chSysHalt();
 
-	dmaStreamSetPeripheral(STM32_DMA2_STREAM6,
+	//FIXME dmaStreamSetPeripheral(STM32_DMA2_STREAM6,
+#endif
 }
 
+/******************************************************************************
+ * LOCAL Functions
+ ******************************************************************************/
+
+#if 0
+
+/* SPI transfer callback, used when buffers need to be rotated */
+static void _dma_rotate_callback( SPIDriver * driver , u32 flags )
+{
+	/*TODO do some magic */
+}
+#endif
