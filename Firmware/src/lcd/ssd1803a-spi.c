ab2d72e547150809975700683562ebf93a4029a4
Ollo <ollo@vm>
2014-11-17 20:53:13 +0100

    describe the initalization process

--- Firmware/src/lcd/ssd1803a-spi.c
+++ Firmware/src/lcd/ssd1803a-spi.c
@@ -60,6 +60,19 @@ __attribute__((noreturn))
   (void) arg;
   chRegSetThreadName("lcd-ssd1803a");
 
+  /** The init procedure */
+/* Command               RS      R/W     DB7     DB6     DB5     DB4     DB3     DB2     DB1     DB0     Hex     Remark
+ * Function Set          0       0       0       0       1        1      1       0       1       0       $3A     8-Bit data length extension Bit RE=1; REV=0
+ * Extended funcion set  0       0       0       0       0        0      1       0       0       1       $09     4 line display
+ * Entry mode set        0       0       0       0       0        0      0       1       1       0       $06     bottom view
+ * Bias setting          0       0       0       0       0        1      1       1       1       0       $1E     BS1=1
+ * Function Set          0       0       0       0       1        1      1       0       0       1       $39     8-Bit data length extension Bit RE=0; IS=1
+ * Internal OSC          0       0       0       0       0        1      1       0       1       1       $1B     BS0=1 -> Bias=1/6
+ * Follower control      0       0       0       1       1        0      1       1       1       0       $6E     Devider on and set value
+ * Power control         0       0       0       1       0        1      0       1       1       1       $57     Booster on and set contrast (DB1=C5, DB0=C4)
+ * Contrast Set          0       0       0       1       1        1      0       0       1       0       $72     Set contrast (DB3-DB0=C3-C0)
+ * Function Set          0       0       0       0       1        1      1       0       1       0       $38     8-Bit data length extension Bit RE=0; IS=0
+ * Display On            0       0       0       0       0        0      1       1       1       1       $0F     Display on, cursor on, blink on */
 
       /*FIXME while (1) */
       {
