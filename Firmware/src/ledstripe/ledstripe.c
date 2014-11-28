24ad4c3c5078eb4de4a96fafd8a0da4001dab235
c3ma <c3ma@calculator>
2014-11-29 00:27:20 +0100

    Beta

--- Firmware/src/ledstripe/ledstripe.c
+++ Firmware/src/ledstripe/ledstripe.c
@@ -106,6 +106,7 @@ void ledstripe_init(void) {
 			STM32_TIM_CCMR2_OC4PE; // OCPreload_Enable
 	STM32_TIM3->CCER = STM32_TIM_CCER_CC4E; /* Not set: STM32_TIM_CCER_CC4P */
 
+	STM32_TIM3->CCR[3] = 49;
 	STM32_TIM3->CR1 |= STM32_TIM_CR1_CEN; // TIM3 enable; CR1 is now locked
 
 	// DMA init
