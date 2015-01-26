6ad3f2d4d338952c91123f215f300af7c0a173d0
Ollo <ollo@vm>
2015-01-26 18:27:16 +0100

    Using a stripped down board configuration

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -68,15 +68,10 @@ void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
   /* Note, only in the ADC_COMPLETE state because the ADC driver fires an
      intermediate callback when the buffer is half full.*/
   if (adcp->state == ADC_COMPLETE) {
-    adcsample_t avg_ch1, avg_ch2;
 
     palTogglePad(GPIOD, GPIOD_LED5);	/* Red On*/
 
-    /* Calculates the average values from the ADC samples.*/
-    avg_ch1 = (samples[0] + samples[2] + samples[4] + samples[6]) / 4;
-    avg_ch2 = (samples[1] + samples[3] + samples[5] + samples[7]) / 4;
-
-    value1 = samples;
+    value1 = (uint32_t) samples;
 
   }
 }
@@ -191,11 +186,11 @@ int main(void)
 
 	UPRINT("Initialize ADC ...");
 	/*
-     * The pin PC1 on the port GPIOC is programmed as analog input.
+         * The pin PC1 on the port GPIOC is programmed as analog input.
 	 */
 	adcStart(&ADCD1, NULL);
 	adcSTM32EnableTSVREFE();
-	palSetPadMode(GPIOC, GPIOC_ETH_RMII_MDC, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOC, GPIOC_PIN1, PAL_MODE_INPUT_ANALOG);
 	UPRINT( " Done\r\n");
 
 
