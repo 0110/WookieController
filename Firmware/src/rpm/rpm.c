aae3ce4194a248587bfae543758d5dba270d91c3
Ollo <ollo@vm>
2015-02-03 19:08:42 +0100

    Simple demo prepared

--- Firmware/src/rpm/rpm.c
+++ Firmware/src/rpm/rpm.c
@@ -8,6 +8,10 @@
 
 #include "rpm/rpm.h"
 
+#include "ch.h"
+#include "hal.h"
+
+#include "usbcdc/usbcdc.h"
 
 /******************************************************************************
  * PROTOTYPE
@@ -29,6 +33,21 @@
 
 rpm_ret_t rpm_init(void)
 {
+   while(TRUE)
+   {
+       if (palReadPad(GPIOA, GPIOA_BTNRPM1))
+       {
+               palSetPad(GPIOD, GPIOD_LED5);   /* Red On*/
+               usbcdc_print("Peep\r\n");
+       }
+       else
+       {
+         palClearPad(GPIOD, GPIOD_LED5);   /* Red Off*/
+       }
+       /* Wait some time, to make the scheduler running tasks with lower prio */
+       chThdSleep(MS2ST(50));
+   }
+
    /*TODO fill in here the magic */
    return RET_ERROR;
 }
