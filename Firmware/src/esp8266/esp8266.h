ffd5399e3640ee7a305bd50bea421c4c9e5df3ac
c3ma <c3ma@calculator>
2015-02-24 22:02:44 +0100

    A little bit structure in the code

--- Firmware/src/esp8266/esp8266.h
+++ Firmware/src/esp8266/esp8266.h
@@ -1,14 +1,22 @@
-/*
- * esp8266.h
- *
- * Created on: Feb 24, 2015
- * Author: c3ma
+/** @file esp8266.h
+ * @brief Module to provide an WLAN connection via UART
+ * @page ESP8266 UART WLAN
+ * @author Ollo
+ * @date 24.02.2015
  *
  * Source of commands: http://defcon-cc.dyndns.org/wiki/ESP8266#Software
+ *
+ * @defgroup ESP6266 Output library (USB UART)
+ * @{
+ *
  */
 
 #ifndef ESP8266_H_
 #define ESP8266_H_
+
+/******************************************************************************
+ * INCLUDES
+ ******************************************************************************/
 
 #include "mcuconf.h"
 
@@ -16,11 +24,21 @@
 #error "We need the second UART for the WLAN dongle"
 #endif
 
+/******************************************************************************
+ * DEFINES
+ ******************************************************************************/
+
+/******************************************************************************
+ * GLOBAL FUNCTIONS
+ ******************************************************************************/
+
 /** @fn void esp8266_init(char *ssid, char *password)
  * @param[in] ssid     the ssid of the network to connect to
  * @param[in] password the password of ssid
  * @brief initialization of the UART Wifi module.
  */
 void esp8266_init(char *ssid, char *password);
+
+/*@}*/
 
 #endif /* ESP8266_H_ */
