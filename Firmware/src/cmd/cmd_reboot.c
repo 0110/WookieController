4b7dfc55e745071a3922cc0ac43304646917c4c0
Ollo <ollo@vm>
2015-06-18 18:32:12 +0200

    Reboot the board

--- Firmware/src/cmd/cmd_reboot.c
+++ Firmware/src/cmd/cmd_reboot.c
@@ -24,11 +24,10 @@ void cmd_reboot(BaseSequentialStream *chp, int argc, char *argv[])
 
   /* Restart the board */
   chSysLock();
-	  /*SCB->AIRCR = SCB_AIRCR_SYSRESETREQ_Msk; */
-	  /*0x05fa0000 | SCB_AIRCR_VECTKEY_Pos*/;
-	  SCB->AIRCR = ((0x5FA << SCB_AIRCR_VECTKEY_Pos) | (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) |
+  SCB->AIRCR = ((0x5FA << SCB_AIRCR_VECTKEY_Pos) | (SCB->AIRCR & SCB_AIRCR_PRIGROUP_Msk) |
 			  SCB_AIRCR_VECTRESET_Msk | SCB_AIRCR_SYSRESETREQ_Msk);
-	  while(1);
+  while(1);
+  /* Source: https://my.st.com/public/STe2ecommunities/mcu/Lists/cortex_mx_stm32/Flat.aspx?RootFolder=https%3a%2f%2fmy.st.com%2fpublic%2fSTe2ecommunities%2fmcu%2fLists%2fcortex_mx_stm32%2fSystem%20software%20reset%2c%20effects%20and%20privileges&FolderCTID=0x01200200770978C69A1141439FE559EB459D7580009C4E14902C3CDE46A77F0FFD06506F5B&currentviews=2290 */
 }
 
 void cmd_poweroff(BaseSequentialStream *chp, int argc, char *argv[])
