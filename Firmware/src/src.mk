c274562356673c6371ccd552ff67891e9363be95
Ollo <Ollo@dev1>
2015-10-03 14:12:12 +0200

    Renamed module for refactoring

--- Firmware/src/src.mk
+++ Firmware/src/src.mk
@@ -1,7 +1,7 @@
 # List of all the board related files.
 APPSRC = src/main.c \
 		src/usbcdc/usbcdc.c \
-		src/lcd/ssd1803a-spi.c \
+		src/lcd/ea_dogl128x-6.c \
 		src/lcd/spi-implement.c \
 		src/cmd/cmd_threads.c \
 		src/cmd/cmd_mem.c 
