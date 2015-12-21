0a852e7cfa6bf501715db383d785b09db561947c
Ollo <Ollo@dev1>
2015-12-21 17:17:08 +0100

    Integrated the old implementation

--- Firmware/src/board_ST7565.h
+++ Firmware/src/board_ST7565.h
@@ -17,8 +17,47 @@
  * #define ST7565_PAGE_ORDER       4,5,6,7,0,1,2,3
  */
 
+#include "ch.h"
+#include "hal.h"
+
+/*
+ * SPI end transfer callback.
+ */
+static void spicb(SPIDriver *spip)
+{
+  (void) spip;
+}
+
+/*
+ * SPI configuration structure.
+ * The slave select line is the pin GPIOA_SPI1NSS on the port GPIOA.
+ */
+static const SPIConfig spicfg = {
+  spicb,
+  /* HW dependent part.*/
+  GPIOB,
+  12,
+  SPI_CR1_BR_2
+};
+
 static GFXINLINE void init_board(GDisplay *g) {
 	(void) g;
+  /*
+   * Initializes the SPI driver 2. The SPI2 signals are routed as follow:
+   * PB12 - NSS.
+   * PB13 - SCK.
+   * PB14 - MISO.
+   * PB15 - MOSI.
+   */
+  spiStart(&SPID2, &spicfg);
+  palSetPad(GPIOB, 12);
+  palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL |
+                           PAL_STM32_OSPEED_HIGHEST);           /* NSS.     */
+  palSetPadMode(GPIOB, 13, PAL_MODE_ALTERNATE(5) |
+                           PAL_STM32_OSPEED_HIGHEST);           /* SCK.     */
+  palSetPadMode(GPIOB, 14, PAL_MODE_ALTERNATE(5));              /* MISO.    */
+  palSetPadMode(GPIOB, 15, PAL_MODE_ALTERNATE(5) |
+                           PAL_STM32_OSPEED_HIGHEST);           /* MOSI.    */
 }
 
 static GFXINLINE void post_init_board(GDisplay *g) {
@@ -47,6 +86,7 @@ static GFXINLINE void write_data(GDisplay *g, uint8_t* data, uint16_t length) {
 	(void) g;
 	(void) data;
 	(void) length;
+	spiStartSendI(&SPID2, length, data);
 }
 
 #endif /* _GDISP_LLD_BOARD_H */
