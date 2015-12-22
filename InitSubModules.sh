17ab7303a789395d0d15ec046f366087cf80ef69
Ollo <Ollo@dev1>
2015-12-22 23:13:40 +0100

    Patch always the files (do not ask if they are overwritten)

--- InitSubModules.sh
+++ InitSubModules.sh
@@ -1,5 +1,5 @@
 git submodule init
 git submodule update
 cd Firmware/lib/ChibiOS/ext/
-unzip fatfs-0.9-patched.zip
-unzip lwip-1.4.1_patched.zip
+unzip -o fatfs-0.9-patched.zip
+unzip -o lwip-1.4.1_patched.zip
